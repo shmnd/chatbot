81a557bc06afc6e18f517a16002ed083a49c0dfa
Shamnad <hamzashamnad123@gmail.com>
2025-04-08 18:41:14 +0530

    basic1

--- whatsapp_backend/whatsapp_backend/settings.py
+++ whatsapp_backend/whatsapp_backend/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
