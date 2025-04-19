8a48144767abaab0b576e0b57e9a45b3f35770bc
Shamnad <hamzashamnad123@gmail.com>
2025-04-19 11:14:32 +0530

    edit js in filter

--- whatsapp_backend/filter/views.py
+++ whatsapp_backend/filter/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Filter
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 
@@ -16,16 +18,26 @@ def filter_list_create_view(request):
     return render(request, "filter/filter_page.html", {"filters": filters})
 
 def delete_filter(request, pk):
-    print(pk,'idddddddddddd')
+    print(pk,'delete idddddddddddd')
     filter_obj = get_object_or_404(Filter, pk=pk)
     filter_obj.delete()
     return JsonResponse({"status": "deleted"})
 
+@csrf_exempt
 def update_filter(request, pk):
     filter_obj = get_object_or_404(Filter, pk=pk)
+
     if request.method == "POST":
         name = request.POST.get("filter_name")
         filter_obj.filter_name = name
         filter_obj.save()
-        return JsonResponse({"status": "updated"})
+        
+
+        filters = Filter.objects.all()
+        return render(request, "filter/filter_page.html", {
+            "filters": filters
+        })
+
+
+    # For GET request, return current filter name
     return JsonResponse({"filter_name": filter_obj.filter_name})
