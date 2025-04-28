33699066077b16b7338ca750b6f88f750fb322cb
Shamnad <hamzashamnad123@gmail.com>
2025-04-28 18:51:13 +0530

    status removed in modal

--- whatsapp_backend/whatsapp/models.py
+++ whatsapp_backend/whatsapp/models.py
@@ -64,7 +64,7 @@ class WhatsAppTemplate(AbstractDateFieldMix):
     media_url = models.URLField(max_length=10000,blank=True, null=True)
     variable_count = models.IntegerField(default=0,null=True,blank=True)
     description = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=50, null=True, blank=True)
+
 
     def __str__(self):
         return f"{self.template_name} "#({self.category.name})"
